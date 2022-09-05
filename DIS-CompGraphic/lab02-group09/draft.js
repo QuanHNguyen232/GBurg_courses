02835b3f0f5fb79bc742c8fc818d91355e41bdf9
QuanHNguyen232 <quan.nh2002@gmail.com>
2022-09-05 18:22:31 +0200

    Change DIS Comp Graph to new repo

--- DIS-CompGraphic/lab02-group09/draft.js
+++
@@ -1,9 +0,0 @@
-// add an event listener that calls keyDownFunction
-// whenever any key is pressed.
-window.addEventListener("keydown", keyDownFunction);
-
-function keyDownFunction(event){
-    if(event.key === "ArrowDown"){
-        console.log("pressed down")
-    }
-}
